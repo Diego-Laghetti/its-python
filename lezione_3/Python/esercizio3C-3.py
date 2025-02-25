a2e40d8cbc42d71b413a2f6d8099e15e76a4ca1c
Diego-Laghetti <diegolaghetti@gmail.com>
2025-02-25 17:03:59 +0100

    esercizio

---
+++ lezione_3/Python/esercizio3C-3.py
@@ -0,0 +1,19 @@
+#esercizio3c-3
+oggetti = []
+x=str()
+
+while len(oggetti) < 3:
+    x=str(input("Inserisci oggetto "))
+    oggetti.append(x)
+
+match oggetti:
+    case ["penna", "matita", "quaderno"]:
+        print("Materiale scolastico")
+    case ["pane","latte","uova"]:
+        print("Prodotti alimentari")
+    case ["sedia", "tavolo", "armadio"]:
+        print("Mobili")
+    case ["telefono", "computer", "tablet"]:
+        print("Dispositivi elettronici")
+    case _:
+        print("Categoria sconosciuta")
