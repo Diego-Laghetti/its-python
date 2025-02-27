4ecdcfaed6bff50bfef7cfe4b444f3ac0ce2d594
Diego-Laghetti <diegolaghetti@gmail.com>
2025-02-27 15:27:18 +0100

    esercizio

--- lezione_3/Python/esercizio3C-7.py
+++ lezione_3/Python/esercizio3C-7.py
@@ -20,4 +20,6 @@ perct=testa/8*100
 percc=croce/8*100
 print(f"la percentuale delle volte in cui è uscita testa è {perct}%")
 print(f"la percentuale delle volte in cui è uscita croce è {percc}%")
+print(f"la percentuale delle volte in cui è uscita testa è {testa}/8")
+print(f"la percentuale delle volte in cui è uscita croce è {croce}/8")
 
