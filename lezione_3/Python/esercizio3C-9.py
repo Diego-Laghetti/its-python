440e45d8392d930994eda113e926c6a4ab7e75ac
Diego-Laghetti <diegolaghetti@gmail.com>
2025-02-27 16:17:36 +0100

    esercizio

---
+++ lezione_3/Python/esercizio3C-9.py
@@ -0,0 +1,21 @@
+#esercizio3C-9
+x = int(input("Inserisci la coordinata X: "))
+y = int(input("Inserisci la coordinata Y: "))
+punto = (x, y)
+match punto:
+    case (0, 0):
+        print(f"Il punto {punto} si trova nell'origine.")
+    case (x, 0):
+        print(f"Il punto {punto} si trova sull'asse X.")
+    case (0, y):
+        print(f"Il punto {punto} si trova sull'asse Y.")
+    case (x, y) if x > 0 and y > 0:
+        print(f"Il punto {punto} si trova nel primo quadrante.")
+    case (x, y) if x < 0 and y > 0:
+        print(f"Il punto {punto} si trova nel secondo quadrante.")
+    case (x, y) if x < 0 and y < 0:
+        print(f"Il punto {punto} si trova nel terzo quadrante.")
+    case (x, y) if x > 0 and y < 0:
+        print(f"Il punto {punto} si trova nel quarto quadrante.")
+    case _:
+        print("Coordinate non valide.")
