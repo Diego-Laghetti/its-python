8dd77605610afd654a503622cbb182b142575c81
Diego-Laghetti <diegolaghetti@gmail.com>
2025-02-27 15:01:13 +0100

    esercizio

--- lezione_3/Python/esercizio3C-6.py
+++ lezione_3/Python/esercizio3C-6.py
@@ -2,8 +2,8 @@
 mammiferi:list =["cane", "gatto", "cavallo", "elefante", "leone", "balena", "delfino"]
 rettili:list =["serpente", "lucertola", "tartaruga", "coccodrillo"]
 uccelli:list =["aquila", "pappagallo", "gufo", "falco", "cigno", "anatra", "gallina", "tacchino"]
-pesci:list =["squalo", "trota", "salmone", "carpa"]
-acqua: list =["squalo", "trota", "salmone", "carpa"]
+pesci:list =["squalo", "trota", "salmone", "carpa", "delfino"]
+acqua: list =["squalo", "trota", "salmone", "carpa", "delfino"]
 terra: list =["cane", "gatto", "cavallo", "elefante", "leone", "serpente", "lucertola", "tartaruga", "coccodrillo", "aquila", "pappagallo", "gufo", "falco", "cigno", "anatra", "gallina", "tacchino"]
 aria: list =["aquila", "pappagallo", "gufo", "falco",  "anatra"]
 
@@ -14,13 +14,13 @@ habitat= str(input("Inserisci habitat "))
 
 match animale:
     case animale if animale in mammiferi:
-        animal_type == "mammiferi"
+        animal_type = "mammiferi"
     case animale if animale in rettili:
-        animal_type == "rettili"
+        animal_type = "rettili"
     case animale if animale in uccelli:
-        animal_type == "uccelli"
+        animal_type = "uccelli"
     case animale if animale in pesci:
-        animal_type == "pesci"
+        animal_type = "pesci"
     case _:
         print("non va bene")
 
@@ -29,3 +29,15 @@ animale_info: dict ={"nome": animale, "tipo_animale": animal_type, "habitat": ha
 match animale_info:
     case animale_info if animale_info["habitat"]=="terra" and animale_info["nome"] in terra and animale_info["tipo_animale"] == "mammiferi":
         print(f"{animale} è uno dei mammiferi che vive sulla terra")
+    case animale_info if animale_info["habitat"]=="aria" and animale_info["nome"] in aria and animale_info["tipo_animale"] == "uccelli":
+        print(f"{animale} è uno dei uccelli che vive in aria")
+    case animale_info if animale_info["habitat"]=="acqua" and animale_info["nome"] in acqua and animale_info["tipo_animale"] == "pesci":
+        print(f"{animale} è uno dei pesci che vive nell'acqua")
+    case animale_info if animale_info["habitat"]=="terra" and animale_info["nome"] in terra and animale_info["tipo_animale"] == "rettili":
+        print(f"{animale} è uno dei rettili che vive sulla terra")
+    case animale_info if animale_info["habitat"]=="terra" and animale_info["nome"] in terra and animale_info["tipo_animale"] == "uccelli":
+        print(f"{animale} è uno degli uccelli che vive sulla terra")
+    case animale_info if animale_info["habitat"]=="acqua" and animale_info["nome"] in acqua and animale_info["tipo_animale"] == "mammiferi":
+        print(f"{animale} è uno dei mammiferi che vive in acqua")
+    case _:
+        print("Non sono in grado di associare questo animale ad un habitat specifico")
