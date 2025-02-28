5245efb37eb8a7d358cc56e60ffd2366c392a312
Diego-Laghetti <diegolaghetti@gmail.com>
2025-02-28 15:53:23 +0100

    esercizio

---
+++ lezione_4/esercizio_somma_numeri.py
@@ -0,0 +1,14 @@
+#esercizio_somma_numeri
+sum: int = 0
+for i in range(1, 11):
+    sum += i
+print("La somma dei numeri che vanno da 1 a 10 è:", sum)
+sum: int = 0
+for i in range(20, 38):
+    sum += i
+print("La somma dei numeri che vanno da 20 a 37 è", sum)
+sum: int = 0
+
+for i in range(35, 50):
+    sum += i
+print("La somma dei numeri che vanno da 35 a 50 è", sum)
