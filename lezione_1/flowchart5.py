6c6882bafa2df7da85010af96c7340faf35ed7ea
Diego-Laghetti <diegolaghetti@gmail.com>
2025-03-03 13:25:33 +0100

    esercizio

--- lezione_1/flowchart5.py
+++ lezione_1/flowchart5.py
@@ -1,10 +1,13 @@
-#flowchart5
+#flowchart5_1
 n= int(input("Leggi n: "))
 if n<2:
+    print(f"Il numero non è primo")
+else:
     div=2
 while div < n:
     if n%div==0:
         print("Il numero non è primo")
+        break
     else:
         div=div+1
 else:
