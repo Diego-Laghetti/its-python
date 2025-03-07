491345ba9a819a6986123541d86b1e0ed45a3e47
Diego-Laghetti <diegolaghetti@gmail.com>
2025-03-07 12:37:46 +0100

    esercizio

---
+++ lezione_1/flowchart9.py
@@ -0,0 +1,19 @@
+#flowchart9
+nome = str(input("Inserisci nome: "))
+vendite = int(input("Inserisci vendite: "))
+max_nome= nome
+max=vendite
+min_nome= nome
+min = vendite
+cont = 0
+for i in range (2):
+    new_nome = str(input("Inserisci nuovo nome: "))
+    new_vendite = int(input("Inserisci nuove vendite: "))
+    if new_vendite > max:
+        max_nome = new_nome
+        max = new_vendite
+    elif new_vendite < min:
+        min_nome = new_nome
+        min = new_vendite
+print(f"La persona con più vendite è {max_nome} con {max} vendite")
+print(f"La persona con meno vendite è {min_nome} con {min} vendite")
