afbff5673f183db74f131f7c626c68da290931a6
Diego-Laghetti <diegolaghetti@gmail.com>
2025-03-07 13:25:20 +0100

    esercizio

--- lezione_1/flowchart11.py
+++ lezione_1/flowchart11.py
@@ -3,21 +3,21 @@ liberi = 20
 while True:
     opzione = str(input("Inserisci opzione: "))
     match opzione:
-        case opzione if opzione == "prenota":
+        case "prenota":
             if liberi > 0:
                 liberi -= 1
             else:
                 print("Non ci sono posti disponibili")
-        case opzione if opzione == "libera":
+        case "libera":
             if liberi < 20:
                 liberi += 1
             else:
                 print("Tutti i posti sono già disponibili")
-        case opzione if opzione == "visualizza":
+        case "visualizza":
             print("liberi", liberi)
             pinko= 20 - liberi
             print("occupati", pinko)
-        case opzione if opzione == "esci":
+        case "esci":
             break
         case _:
             continue
