8ab31e1876cd53e4ea60e6ea69b2d7bb9cf4c37f
Diego-Laghetti <diegolaghetti@gmail.com>
2025-04-08 19:00:25 +0200

    ESERCIZI

--- PYTHON/lezione_13/esercizio8.py
+++ PYTHON/lezione_13/esercizio8.py
@@ -1,1 +1,10 @@
 #esercizio8
+def vowelsCounter(s: str) -> int:
+    if not s: 
+        return 0
+    if s[0].lower() in ["a", "e", "i", "o", "u"]:
+        return 1 + vowelsCounter(s[1:])
+    else:
+        return vowelsCounter(s[1:])  
+
+print(f"La stringa \"CIAO\" contiene: {vowelsCounter('CIAO')} vocali")
