828d49bd549ed665be3f93cba296c1202d2a258d
Diego-Laghetti <diegolaghetti@gmail.com>
2025-05-08 18:39:53 +0200

    ESERCIZI

--- PYTHON/lezione_15/example.py
+++ PYTHON/lezione_15/example.py
@@ -1,11 +1,11 @@
 PATH: str = "ITS/PYTHON/lezione_15/example.txt"
-mode: str = "r"
+mode: str = "w"
 encoding: str = "utf-8"
 
 file = open(PATH, mode)
 
 print(file)
 #input_text: str = input("Enter text to write to the file: ")
-output: str = file.read()
+output: str = file.write("forza lazio")
 print(output)
 file.close()
