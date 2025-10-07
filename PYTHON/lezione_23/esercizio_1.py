b8a434a16aecd1e63bdc302badfe474ea1adf829
Diego-Laghetti <diegolaghetti@gmail.com>
2025-10-07 18:42:55 +0200

    esercizi

--- PYTHON/lezione_23/esercizio_1.py
+++ PYTHON/lezione_23/esercizio_1.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__) #istanza della classe
-app.run(debug=True, host='127.0.0.1', port=5000)
-#In production, set debug to False
+'''app.run(debug=True) #fa partire il server (l'app)'''
+
+#path statico
+@app.route('/') 
+def home() -> str: #funzione per definire l'home page
+    return "<h3>Hello world!</h3>"
+
+#path dinamico (url dinamico)
+@app.route('/user/<string:username>/age/<int:age>')
+def show_user_profile(username: str, age: int) -> str:
+    return f"Profilo di {username}, età: {age}"
+
+with app.test_request_context():
+    print(url_for('home'))
+    print(url_for('show_user_profile', username='playdido', age= 20))
